e052d4436577d1012a45740ddae0185f94dbbf7d
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-09 18:41:04 +0100

    Update Operation.cpp

--- Base/Operation.cpp
+++ Base/Operation.cpp
@@ -37,12 +37,20 @@ int Binary(int n)
     while(n > 0)
     {
         if(n % 2 ==1)
-        {
             c++;
-        }
         n=n/2;
     }
     return c;
+}
+int hex(int n)
+{
+    int x =0, y =0;
+    while(n >0)
+    {
+        x+=(n % 10)*pow(16, y++);
+        n = n/2;
+    }
+    return Binary(x);
 }
 int main() {
     long long int n;
@@ -53,5 +61,6 @@ int main() {
     cout<<"\nReverse of the number is: "<<rev(n);
     cout<< "\nLuck is: " <<luck(n);
     cout<< "\nImplemented Binary: " << Binary(n);
+    cout<< "\nImplemented Hex: " << hex(n);
     return 0;
 }
