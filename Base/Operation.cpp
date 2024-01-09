bef07216210f18f9bb263d6242df21d506082d30
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-09 16:24:11 +0100

    Update Operation.cpp

--- Base/Operation.cpp
+++ Base/Operation.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
 
+int c=0;
 int fact(long long int x) {
     if (x == 0 || x == 1)
         return 1;
     return x * fact(x - 1);
 }
-
 int countDigits(int x) {
-    int c = 0;
     while (x != 0) {
         x = x / 10;
         c++;
     }
     return c;
 }
-
+int rev(int n)
+{
+    int odd;
+    if( n==0)
+        return odd;
+    odd=(n/10)+(n%10)*pow(10, c++);
+}
 int main() {
     long long int n;
     cin >> n;
     cout << "\nFactorial of that number is: " << fact(n) ;
     
     cout << "\nTotal digits of that number is: " <<countDigits(n) ;
+    cout<<"\nReverse of the number is: "<<rev(n);
     return 0;
 }
 
