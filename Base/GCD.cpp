94552a59062b105ee3b651b8d20fa2f0d4d41580
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-09 17:34:49 +0100

    Update GCD.cpp

--- Base/GCD.cpp
+++ Base/GCD.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<bits/stdc++.h>
 #define INF 1e18;
 using namespace std;
  
@@ -9,6 +10,17 @@ ll gcd(ll a, ll b) {
 	if (a == 0 || b == 0)return a + b;
 	if (a > b)return gcd(a%b, b);
 	return gcd(a, b%a);
+}
+ll gcd(ll a, ll b) {
+    if(b > a) 
+        swap(b, a);
+    if(a == b)
+        return a;
+    for(int i = b; i>0; i--)
+    {
+        if(b % i==0 && a % i==0)
+            return i;
+    }
 }
 int main()
 {
