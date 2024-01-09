e58136f8fca10597c825aaf35131c0f540420836
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-09 17:41:41 +0100

    Update GCD.cpp

--- Base/GCD.cpp
+++ Base/GCD.cpp
@@ -1,30 +1,42 @@
 #include<iostream>
 #include<vector>
 #include<bits/stdc++.h>
-#define INF 1e18;
+#define INF 1e18
 using namespace std;
  
 typedef long long ll;
 typedef long double ld;
+
 ll gcd(ll a, ll b) {
-	if (a == 0 || b == 0)return a + b;
-	if (a > b)return gcd(a%b, b);
-	return gcd(a, b%a);
+    if (a == 0 || b == 0) return a + b;
+    if (a > b) return gcd(a % b, b);
+    return gcd(a, b % a);
 }
+
 ll gcd(ll a, ll b) {
-    if(b > a) 
+    if (b > a) 
         swap(b, a);
-    if(a == b)
+    if (a == b)
         return a;
-    for(int i = b; i>0; i--)
-    {
-        if(b % i==0 && a % i==0)
+    for (int i = b; i > 0; i--) {
+        if (b % i == 0 && a % i == 0)
             return i;
     }
 }
-int main()
-{
-    ll n, x;
-    cin >> n >>x;
-    cout<< gcd(n, x);
+
+ll lcm(ll a, ll b) {
+    for (int i = max(a, b); i <= a * b; i++) {
+        if (i % a == 0 && i % b == 0)
+            return i;
+    }
+    return -1;
 }
+
+int main() {
+    ll n, x;
+    cin >> n >> x;
+    cout << gcd(n, x) << endl;
+    cout << lcm(n, x) << endl;
+    return 0;
+}
+
