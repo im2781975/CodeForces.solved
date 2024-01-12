729e4d1e31660f2fb77eaecb5d848e4dfc114c55
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 19:01:41 +0100

    Update Implement 2.cpp

--- Array/Implement 2.cpp
+++ Array/Implement 2.cpp
@@ -43,3 +43,26 @@ int main(){
     }
     return 0;
 }
+int main(){
+    int t;
+    cin >> t;
+    while(t){
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        sort(arr, arr+n, greater<int>());
+        if(n == 1){
+            if(arr[0] > 1) cout << "NO" << endl;
+            else cout << "YES" << endl;
+            t--;
+            continue;
+        }
+        if(arr[0] - arr[1] <= 1) cout << "YES" << endl;
+        else cout << "NO" << endl;
+        t--;
+    }
+    return 0;
+}
