47747a636fafa9a95242154a52a73835873a38d9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 16:37:35 +0100

    Update Implement 4.cpp

--- Vector/Implement 4.cpp
+++ Vector/Implement 4.cpp
@@ -36,3 +36,61 @@ void solveContest1790ProblemB()
     }
     cout << "\n";
   }
+void solveDiv2A()
+  {
+    ll n;
+    bool flag = true;
+    ll temp = 9999999999999999;
+    cin >> n;
+    vector<ll> v(n);
+    for (int i = 0; i < n; i++)
+      cin >> v[i];
+    for (int i = 1; i < n; i++)
+    {
+      if (v[i] < v[i - 1])
+      {
+        flag = false;
+        break;
+      }
+    }
+    if (flag)
+    {
+      for (int i = 1; i < n; i++)
+      {
+        temp = min(temp, v[i] - v[i - 1]);
+      }
+      cout << temp / 2 + 1 << "\n";
+    }
+    else
+      cout << 0 << "\n";
+  }
+  void div2BFibonachairs()
+  {
+    ll n, k, ans = 0, t = 0, t2 = 0;
+    cin >> n >> k;
+    ll temp = k;
+    vector<ll> v;
+    int total = 0;
+    for (int i = 0; i < n; i++)
+    {
+      ll t3 = 0;
+      t = n;
+      t2 = n - i;
+      for (int j = 2; j < k; j++)
+      {
+        if (t3 >= 0)
+        {
+          t3 = t - t2;
+          t = t2;
+          t2 = t3;
+        }
+        else
+          break;
+      }
+      if (t3 >= 0 && t2 <= t)
+      {
+        ans++;
+      }
+    }
+    cout << ans << "\n";
+  }
