5e3a2c967315d04303d106ab2b9ae0d1d30a8ac0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 20:13:10 +0100

    Update Implement 3.cpp

--- Array/Implement 3.cpp
+++ Array/Implement 3.cpp
@@ -32,4 +32,37 @@ int main(){
     }
     return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t){
+        int n , m ;
+        cin>>n>>m;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        if(n>m){
+            cout<<"NO"<<endl;
+            continue;
+        }
+        int count = arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>arr[i+1]){
+                count+=2*arr[i]-arr[i+1];
+            }
+        }
+        if(count+n <= m){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+        t--;
+    }
+    return 0;
+}
 
