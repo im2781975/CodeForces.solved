b1f4290b4225b1fb6c84db5ea476bf04c964c3e3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 08:54:23 +0100

    Update Implement 1.cpp

--- Vector/Implement 1.cpp
+++ Vector/Implement 1.cpp
@@ -81,3 +81,29 @@ void Forbidden_Integer() {
         cout << "NO" << "\n";
     }
 }
+void solveee() {
+    int n;
+    cin >> n;
+    int newn = n / 2;
+    vector<int> v(newn);
+    vector<int> v2(newn);
+    if (n % 2 != 0) {
+        for (int i = 0; i < n / 2; i++) {
+            cin >> v[i];
+        }
+        for (int i = 0; i < n / 2; i++) {
+            cin >> v2[i];
+        }
+    } else {
+        for (int i = 0; i < n / 2; i++) {
+            cin >> v[i];
+        }
+        for (int i = 0; i < n / 2; i++) {
+            cin >> v2[i];
+        }
+    }
+    sort(v.begin(), v.end());
+    sort(v2.begin(), v2.end());
+    cout << (v[newn - 1] + v2[newn - 1]) - (v[0] + v2[0]) << "\n";
+}
+
