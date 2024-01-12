8536cafbc59d858de749aa29d80450140e1856c1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 18:29:55 +0100

    Update Operation 2.cpp

--- String/Operation 2.cpp
+++ String/Operation 2.cpp
@@ -30,3 +30,31 @@ void solveContest1790ProblemA()
         break;
     cout << temp << "\n";
   }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t){
+        int n ;
+        cin>>n;
+        string s ;
+        cin>>s;
+        int ans = 0 ;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0'){
+                if(i+2<n){
+                    if(s[i+1]=='0')ans+=2;
+                    else if(s[i+2]=='0')ans++;
+                }
+                else if(i+1<n){
+                    if(s[i+1]=='0')ans+=2;
+                }
+            }
+        }
+        cout<<ans<<endl;
+        t--;
+    }
+    return 0;
+}
