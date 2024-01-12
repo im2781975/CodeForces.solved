00c7efc391c98dc809affe6a0ccd0a1c20913048
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 20:37:47 +0100

    Update Operation 2.cpp

--- Array/Operation 2.cpp
+++ Array/Operation 2.cpp
@@ -64,4 +64,31 @@ int main(){
     }
     return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t){
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+        }
+        int count = 0;
+        for(int i = 1; i < n; i++){
+            if(arr[i] == arr[i - 1]){
+                count++;
+                i++;
+            }
+        }
+        if(count == 0 || count == 1){
+            // Code to be added here
+        }
+        t--;
+    }
+    return 0;
+}
 
