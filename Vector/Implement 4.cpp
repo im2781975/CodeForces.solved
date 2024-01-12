14db9ac9c3e2c1d8a6647b72234f5abe62e4977f
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 16:36:24 +0100

    Create Implement 4.cpp

---
+++ Vector/Implement 4.cpp
@@ -0,0 +1,38 @@
+void solveContest1790ProblemB()
+  {
+    int n, s, r, temp = 0, temp2 = 1;
+    cin >> n >> s >> r;
+    temp = r;
+    vector<int> v;
+    vector<int> sides = {1, 2, 3, 4, 5, 6};
+    vector<int> result;
+    for (int i = 0; i < n - 1; i++)
+    {
+      for (int j = 5; j >= 0; j--)
+      {
+        while (r >= sides[j] && sides[j] <= s - r)
+        {
+          r -= sides[j];
+          result.pb(sides[j]);
+          temp2++;
+        }
+      }
+    }
+    sort(result.rbegin(), result.rend());
+    for (int i = 0; i < n - 2; i++)
+    {
+      while (result[i] > 1)
+      {
+        if (result.size() == n - 1)
+          break;
+        result[i]--;
+        result.pb(1);
+      }
+    }
+    result.pb(s - temp);
+    for (auto j : result)
+    {
+      cout << j << " ";
+    }
+    cout << "\n";
+  }
