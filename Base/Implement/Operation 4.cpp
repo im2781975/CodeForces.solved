4b09035972046735d4f2653297d15a1e0dfe3594
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 16:53:51 +0100

    Update Operation 4.cpp

--- Base/Implement/Operation 4.cpp
+++ Base/Implement/Operation 4.cpp
@@ -14,3 +14,38 @@ void div2_29_7_23B()
     }
     cout << msize << "\n";
   }
+ll maxe(ll num, ll num2)
+  {
+    if (num > num2)
+      return num;
+    else
+      return num2;
+  }
+  bool isSorted(vector<ll> v)
+  {
+    for (int i = 1; i < v.size(); i++)
+    {
+      if (v[i - 1] > v[i])
+        return false;
+    }
+    return true;
+  }
+  bool checking(string s)
+  {
+    for (int i = 0; i < s.length(); i++)
+    {
+      if (s[i] != '0')
+        return true;
+    }
+    return false;
+  }
+  bool LocalMax(vector<ll> v)
+  {
+    for (int i = 1; i < v.size(); i++)
+    {
+      if (i != v.size() - 1)
+        if (v[i] > v[i - 1] && v[i] > v[i + 1])
+          return false;
+    }
+    return true;
+  }
