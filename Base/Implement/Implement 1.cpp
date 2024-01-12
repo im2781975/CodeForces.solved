b3c92bd5dc878e6a1990e8836b0027b6b5fe1cde
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 20:24:13 +0100

    Update Implement 1.cpp

--- Base/Implement/Implement 1.cpp
+++ Base/Implement/Implement 1.cpp
@@ -108,3 +108,28 @@ int main(){
     }
     return 0;
 }
+
+int n = height.length();
+int left = 0, right = n - 1;
+int res = 0;
+int leftmax = 0, rightmax = 0;
+
+while (left <= right) {
+    if (height[left] <= height[right]) {
+        if (height[left] >= leftmax) {
+            leftmax = height[left];
+        } else {
+            res += leftmax - height[left];
+        }
+    } else {
+        if (height[right] >= rightmax) {
+            rightmax = height[right];
+        } else {
+            res += rightmax - height[right];
+        }
+        right--;
+    }
+}
+return res;
+
+    
