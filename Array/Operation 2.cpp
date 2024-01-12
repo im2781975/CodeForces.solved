f64203d248ed4bc215a60a6384932900b415f368
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 20:36:14 +0100

    Update Operation 2.cpp

--- Array/Operation 2.cpp
+++ Array/Operation 2.cpp
@@ -25,4 +25,43 @@ int main(){
     }
     return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t){
+        string s;
+        cin >> s;
+        int n  = s.length();
+        bool ans = true;
+        if(s[0]=='B' || s[n-1]=='A'){
+            ans = false;
+        }
+        else{
+            int a = 1, b = 0; 
+            for(int i=1; i<n; i++){
+                if(s[i]=='A'){
+                    a++;
+                }
+                else if(s[i]=='B'){
+                    b++;
+                    if(a < b){
+                        ans = false;
+                        break;
+                    }
+                }
+            }
+        }
+        if(ans){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+        t--;
+    }
+    return 0;
+}
 
