7b99cccc4196b7db6ed50c3f9c69280fa083b988
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 19:10:52 +0100

    Update Implement 2.cpp

--- Array/Implement 2.cpp
+++ Array/Implement 2.cpp
@@ -66,3 +66,38 @@ int main(){
     }
     return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t){
+        int n;
+        cin >> n; 
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        for(int j=0; j<n-1; j++){
+            int i=j+1;
+            int mini = INT_MAX;
+            int miniIndex = -1;
+            while(i < n){
+                if(arr[i] < arr[j] && arr[i] < mini){
+                    mini = arr[i];
+                    miniIndex = i;
+                }
+                i++;
+            }
+            if(mini != INT_MAX){
+                reverse(arr+j, arr+miniIndex+1);
+                break;
+            }
+        }
+        for(auto i:arr) cout << i << " ";
+        cout << endl;
+        t--;
+    }
+    return 0;
+}
