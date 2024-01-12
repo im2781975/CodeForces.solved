73cc6f64e22c6ebe87ed29a0ac8e20332bebd599
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 18:52:27 +0100

    Update Implement 1.cpp

--- Base/Implement/Implement 1.cpp
+++ Base/Implement/Implement 1.cpp
@@ -12,4 +12,36 @@ int main(){
     }
     return 0;
 }
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t){
+        long long int n ;
+        cin>> n ;
+        long long int arr[n];
+        for(long long int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        long long int ans =0 ;
+        for(long long int i=1;i<n-1;i++){
+            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+                if(i+2 != n && arr[i+2]>arr[i]){
+                    arr[i+1] = arr[i+2];
+                }
+                else{
+                    arr[i+1] = arr[i];
+                }
+                ans++;
+            }
+        }
+        cout<<ans<<endl;
+        for(auto i:arr)cout<<i<<" ";
+        cout<<endl;
+        t--;
+    }
+    return 0;
+}
 
