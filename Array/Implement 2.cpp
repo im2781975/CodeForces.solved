5f68693232f3dd8dc13faff3e994c652168f57cc
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-12 18:48:03 +0100

    Update Implement 2.cpp

--- Array/Implement 2.cpp
+++ Array/Implement 2.cpp
@@ -26,3 +26,20 @@ int main(){
     }
     return 0;
 }
+int main(){
+    int t;
+    cin >> t;
+    while(t){
+        int n;
+        cin >> n;
+        int arr[n];
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+            ans = ans | arr[i];
+        }
+        cout << ans << endl;
+        t--;
+    }
+    return 0;
+}
