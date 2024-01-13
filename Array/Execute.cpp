888b05038b3e9782fa40a5e7c14bf03f69b4d614
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-13 22:10:50 +0100

    Update Execute.cpp

--- Array/Execute.cpp
+++ Array/Execute.cpp
@@ -125,6 +125,17 @@ void diffrence()
             cout<< "No";
     }
 }
+void OrGate()
+{
+    int n, ans=0;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    for(int i=0; i<n; i++)
+        ans=ans | arr[i];
+    cout << ans;
+}
 int main()
 {
     MinSumDiagonal();
