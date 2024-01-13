496d951098f70b29c164bff79251e752a0f29ebf
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-13 15:49:02 +0100

    Update Execute.cpp

--- Array/Execute.cpp
+++ Array/Execute.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
-    //find the minimum sum of diagonals in a 2D array.
+    //find the minimum sum of diagonals in a 2D array
+    cout<< "Enter row & column number: ";
     int n;
     cin >> n;
     int arr[n][n], result =0;
     int Max=0;
     int MAXROW=INT_MAX;
+    cout<< "\nEnter elements: \n";
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<n; j++)
@@ -16,15 +18,18 @@ int main()
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<n; j++)
+        {
             Max+=arr[i+j][j];
-            
+        }
         MAXROW=min(MAXROW, Max);
         Max=0;
     }
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<n; j++)
+        {
             result=max(result, MAXROW);
+        }
     }
     cout<< result;
 }
