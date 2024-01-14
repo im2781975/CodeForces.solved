7dab4148f76609beaed6f66e46296388030fb540
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-14 18:48:12 +0100

    Create Execute 1.cpp

---
+++ Array/Execute 1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+void Tic_tac_toe(){
+    int n, flag= 0;
+    //Flag to determine if there is a winner (1) or not (0).
+    cout<<"Enter the number of turns: \n";
+    cin >> n;
+    char arr[3][3];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) 
+                cin >> arr[j][k];
+        }
+        if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[0][0] != '.') {
+            flag= 1;
+            cout << arr[0][0] << "\n";
+        } else if (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0] && arr[0][2] != '.') {
+            flag= 1;
+            cout << arr[0][2] << "\n";
+        } else {
+            // Check rows and columns
+            for (int z = 0; z < 3; z++) {
+                if (arr[z][0] == arr[z][1] && arr[z][2] == arr[z][1] && arr[z][1] != '.') {
+                    flag= 1;
+                    cout << arr[z][0] << "\n";
+                    break;
+                }
+                if (arr[0][z] == arr[1][z] && arr[2][z] == arr[0][z] && arr[0][z] != '.') {
+                    flag= 1;
+                    cout << arr[0][z] << "\n";
+                    break;
+                }
+            }
+        }
+        // Draw if no winner
+        if (f == 0) {
+            cout << "DRAW\n";
+        }
+    }
+}
