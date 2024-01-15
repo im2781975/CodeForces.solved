7ccd21801ccf304e9e07f3d71d9069542e6b672e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-15 19:58:30 +0100

    Update Operation 4.cpp

--- Array/Operation 4.cpp
+++ Array/Operation 4.cpp
@@ -113,3 +113,55 @@ int x;
 			if(z!=x) cout<<" --->";
 		}
 		cout<<"\nThe system is in safe-state";
+----
+int x,z=0;
+   cin>>x;
+   int y[x];
+   while(cin>>y[z++]);
+   int l=0,k=x;
+   for(int i=0;i<x;i++)
+   {
+	   if(y[i]){
+		   int m=0;
+		   for(int j=i+1;j<x;j++)
+		   {
+			   if(y[i]==y[j])
+			   {
+				   m++;
+				   y[j]=0;
+			   }
+		   }
+		   if(m>0) k-=m;
+		   if(m>l) l=m;
+	   }
+   }
+   cout<<l+1<<" "<<k;
+----
+int x,z=0,even=0,odd=0,eve=0,od=0;
+    cin>>x;
+    int y[x];
+    while(z<x) cin>>y[z++];
+    z=0;
+    while(z++<x)
+    {
+    	if(y[z-1]%2==0) {even++; eve=z-1;}
+    	else {odd++; od=z-1;}
+    }
+    if(even==1) cout<<eve+1;
+    else cout<<od+1;
+----
+int n,k,z=0,c=0;
+   cin>>n>>k;
+   int y[n];
+   while(cin>>y[z++]);
+   z=0;
+   while(z++<n)
+   {
+	   if(y[z-1]>=y[k-1])
+		   {if(y[z-1]>0)
+			   c++;}
+	   else break;
+   }
+   cout<<c;
+        return 0;
+}
