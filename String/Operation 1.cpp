b9fea07f6e1c1b56c762386388f697d2ac401f09
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-15 19:56:30 +0100

    Update Operation 1.cpp

--- String/Operation 1.cpp
+++ String/Operation 1.cpp
@@ -58,4 +58,69 @@ void solveE()
   temp = 0;
   temp2 = 0;
 }
+----
+string x,y="heavy",z="metal";
+   cin>>x;
+   long long h=-5,m,c=0;
+   for(int i=0;;i++)
+   {
+	   h=x.find(y,h+5);
+	   if(h>-1)
+	   {
+		   m=x.length();
+		   for(int j=0;;j++)
+		   {
+			   m=x.rfind(z,m-1);
+			   if(m>-1&&m>h+4) c++;
+			   else break;
+		   }
+	   }
+	   else break;
+   }
+   cout<<c;
+----
+string s;
+	cin>>s;
+	int z=0,c=0;
+	while(z++<s.size())
+	{
+		if(s[z-1]=='h'&&c==0)
+		{
+			c++;
+			continue;
+		}
+		if(s[z-1]=='e'&&c==1)
+		{
+			c++;
+			continue;
+		}
+		if(s[z-1]=='l'&&c==2)
+		{
+			c++;
+			continue;
+		}
+		if(s[z-1]=='l'&&c==3)
+		{
+			c++;
+			continue;
+		}
+		if(s[z-1]=='o'&&c==4)
+		{
+			c++;
+			break;
+		}
+	}
+	if(c==5) cout<<"YES";
+	else cout<<"NO";
+----
+string s,d,r;
+    int z=0;
+    cin>>s>>d;
+    while(z++<s.size())
+    {
+    	if(s[z-1]==d[z-1]) r+='0';
+    	else r+='1';
+    }
+    cout<<r;
+----
 
