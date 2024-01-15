3208b972172085d84bd7985c1cb636c47bf9feb1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-15 17:33:21 +0100

    Update Execute.cpp

--- Array/Execute.cpp
+++ Array/Execute.cpp
@@ -226,3 +226,23 @@ void CompareSum {
     }
     return 0;
 }
+void consecutive()
+{
+    int n, ans=0;
+    cin >>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin >>arr[i];
+    for(int i=1; i<n-1; i++)
+    {
+        if(arr[i] > arr[i-1] && arr[i] > arr[i+1])
+        {
+            if(i+2!=n && arr[i+2] > arr[i])
+                arr[i+1]=arr[i+2];
+            else
+                arr[i+1]=arr[i];
+            ans++;
+        }
+    }
+    cout <<ans;
+}
