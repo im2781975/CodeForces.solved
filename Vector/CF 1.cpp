56dfb3688f5bc86eac42cd047df7247b4e2ae7f9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-16 16:55:44 +0100

    Update CF 1.cpp

--- Vector/CF 1.cpp
+++ Vector/CF 1.cpp
@@ -1,3 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solveDiv2A()
+{
+    int n;
+    bool flag=true;
+    int tmp=INT_MAX;
+    cin >>n;
+    vector<int>vec(n);
+    for(int i=0; i<n; i++)
+        cin >> vec[i];
+    for(int i=1; i<n; i++)
+    {
+        if(vec[i]< vec[i-1])
+        {
+            flag=false;
+            break;
+        }
+    }
+    if(flag)
+    {
+        for(int i=1; i<n; i++)
+        {
+            tmp=min(tmp, v[i]-vec[i-1]);
+        }
+        cout <<tmp/2 +1;
+    }
+    else
+        cout << 0;
+}
 void solveContest1790ProblemB()
 {
     int n, s, r, temp = 0, temp2 = 1;
