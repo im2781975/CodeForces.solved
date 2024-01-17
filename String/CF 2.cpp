cf7c8e0d476b2666c44626b8effc636549bac6f1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-17 17:37:42 +0100

    Update CF 2.cpp

--- String/CF 2.cpp
+++ String/CF 2.cpp
@@ -87,3 +87,43 @@ void PalindromeAddition() {
     cout << count << " " << s;
     return 0;
 }
+void BasedCondition() {
+    int n, r, b;
+    cin >> n >> r >> b;
+    int k = 0;
+    string ans = "";
+    ans += 'R';
+    int cnt = ceil((float)r / (b + 1));
+    cout << cnt << endl;
+
+    while (k < n) {
+        for (int i = 0; i < cnt && k < n; i++) {
+            ans += 'R';
+            k++;
+        }
+        if (k < n) {
+            ans += 'B';
+            k++;
+        }
+    }
+
+    int d = r / (b + 1);
+    int remm = r % (b + 1);
+
+    ans = "";
+
+    for (int i = 0; i <= b; i++) {
+        for (int i = 0; i < d; i++) {
+            ans += 'R';
+        }
+        if (remm) {
+            ans += 'R';
+            remm--;
+        }
+        if (i != b) ans += 'B';
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
