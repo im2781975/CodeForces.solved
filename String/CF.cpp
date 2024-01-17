2f199d08fc7d84b648ba50f7482feee0f7ada941
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-17 11:36:18 +0100

    Update CF.cpp

--- String/CF.cpp
+++ String/CF.cpp
@@ -145,3 +145,17 @@ void SwitchPattern()
         }
     }
 }
+void ChangeFormat()
+{
+    string s;
+    cin >>s;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]>= 65 && s[i]<=90)
+            s[i]+=32;
+        else if(s[i]>=92 && s[i]<=122)
+            s[i]-=32;
+    }
+    for(int i=0; i<s.size(); i++)
+        cout << s[i];
+}
