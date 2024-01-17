9d7b3b9c0970c483f733fd4b539b0d2b53c82369
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-17 20:05:06 +0100

    Update CF 2.cpp

--- Vector/CF 2.cpp
+++ Vector/CF 2.cpp
@@ -218,3 +218,13 @@ void solve(){
     }
     cout << op << "\n";
 }
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int>v(n);
+    for(int i = 0; i < n; i++)
+        cin >> v[i];
+    sort(v.begin(), v.end());
+    cout << v[n-1] - v[0] << "\n";
+}
