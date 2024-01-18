dab1532a10a99bc8542e16d1fe0dba675409af49
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-18 15:05:50 +0100

    Update CF 3.cpp

--- Vector/CF 3.cpp
+++ Vector/CF 3.cpp
@@ -88,4 +88,78 @@ void solve4()
         cout << vec[i] <<" ";
     cout << "\n";
     // cout << charac(v, v.size()) << "\n";
-  }
+}
+void solve5()
+{
+    // https://codeforces.com/contest/1714/problem/A
+    int n, h, m, temp = 100;
+    cin >> n >> h >> m;
+    vector<pair<int, int>> v(n);
+    vector<pair<int, int>> result(n);
+    vector<int> sum;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+        int AlarmTime = v[i].first * 60 + v[i].second;
+        int BedTime = (h * 60) + m;
+        int Total = abs(AlarmTime - BedTime) / 60;
+        int Time_in_min = abs(BedTime - AlarmTime) - Total * 60;
+        result[i].first = Total;
+        result[i].second = Time_in_min;
+        if (Total > 12)
+        {
+            Total = (((24 * 60) - BedTime) + AlarmTime) / 60;
+            Time_in_min = abs((24 * 60 - (h * 60 + m) + AlarmTime) - Total * 60);
+            result[i].first = (((24 * 60) - BedTime) + AlarmTime) / 60;
+            result[i].second = abs((24 * 60 - (h * 60 + m) + AlarmTime) - Total * 60);
+        }
+        else if (v[i].first < h || h == v[i].first && m > v[i].second)
+        {
+            int anaT3bt = 24 * 60 - ((h * 60 + m) - (v[i].first * 60 + v[i].second));
+            result[i].first = anaT3bt / 60;
+            result[i].second = abs(60 - Time_in_min);
+        }
+    }
+    sort(result.begin(), result.end());
+    cout << result[0].first << " " << result[0].second << "\n";
+}
+void solve6()
+{
+    // https://codeforces.com/contest/1690/problem/B
+    int n;
+    cin >> n;
+    int temp = 0;
+    int count = 0;
+    bool f = false;
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i].first;
+    for (int i = 0; i < n; i++)
+      cin >> v[i].second;
+      
+    sort(v.begin(), v.end());
+    temp = v[n - 1].first - v[n - 1].second;
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i].second > v[i].first)
+        {
+            f = false;
+            break;
+        }
+        if (v[i].first - temp < 0)
+            v[i].first = 0;
+        else
+            v[i].first -= temp;
+        if (v[i].first == v[i].second)
+            f = true;
+        else
+        {
+            f = false;
+            break;
+        }
+    }
+    if (f)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
