e432a1a99cde943b26baa40690deebb0a669ed44
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-18 15:30:08 +0100

    Update CF 3.cpp

--- String/CF 3.cpp
+++ String/CF 3.cpp
@@ -23,3 +23,31 @@ void solve()
         s = x;
     }
 }
+void solve2() {
+    string s;
+    getline(cin, s);
+    
+    unsigned int i;
+    for(i = 0; i < s.size(); i++)
+        s[i] = tolower(s[i]);
+    
+    for(i = 0; i < s.size(); i++) {
+        if(s[i] < 97 || s[i] > 122) {
+            s.erase(i, 1);
+            i--;
+        }
+    }
+    unsigned int z = 0, c = 0, f = s.size() - 1;
+    z = 0;
+    while (z++ < s.size() / 2) {
+        if (s[z - 1] != s[f--]) {
+            c++;
+            break;
+        }
+    }
+    if (c == 0)
+        cout << "I'll make the saddle";
+    else
+        cout << "Alas, Jon, You failed my test";
+    return 0;
+}
