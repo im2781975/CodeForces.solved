61f4860a228547627f15b4fe99c7caf8ed48ca44
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-18 16:38:10 +0100

    Update CF 1.cpp

--- String/CF 1.cpp
+++ String/CF 1.cpp
@@ -184,6 +184,39 @@ void BasedCondition(){
 
     return 0;
 }
+void solve_ballonIcpc()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int tmp = 0;
+    char arr[50] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[s[i]] == 0)
+            tmp += 2;
+        else
+            tmp += 1;
+        arr[s[i]]++;
+    }
+  cout << tmp << "\n";
+}
+void solveContest1790ProblemA()
+{
+    string s = "314159265358979323846264338327";
+    string ns;
+    cin >> ns;
+    int temp = 0;
+    for (int i = 0; i < ns.length(); i++)
+    {
+        if (ns[i] == s[i])
+            temp++;
+        else
+            break;
+    }
+    cout << temp << "\n";
+}
 int main()
 {
     string s;
