8a7ee8a2db343f7851fc78dd1d5ad8638b6588a5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-18 16:12:12 +0100

    Update CF 3.cpp

--- String/CF 3.cpp
+++ String/CF 3.cpp
@@ -200,3 +200,34 @@ void problem3() {
             cout << "NO\n";
     }
 }
+void solveE()
+{
+    string s, s2;
+    cin >> s >> s2;
+    int temp = 0;
+    int temp2 = 0;
+    int len1 = s.length(), len2 = s2.length();
+    if (s[len1 - 1] == 'M')
+        temp = 1;
+    else if (s[len1 - 1] == 'L')
+        temp = (len1)*2;
+    else if (s[len1 - 1] == 'S')
+        temp = len1 * -1;
+
+    if (s2[len2 - 1] == 'M')
+        temp2 = 1;
+    else if (s2[len2 - 1] == 'L')
+        temp2 = len2 * 2;
+    else if (s2[len2 - 1] == 'S')
+        temp2 = len2 * -1;
+
+  // sec string
+    if (temp > temp2)
+        cout << ">" << "\n";
+    else if (temp < temp2)
+        cout << "<" << "\n";
+    else
+        cout << "=" << "\n";
+    temp = 0;
+    temp2 = 0;
+}
