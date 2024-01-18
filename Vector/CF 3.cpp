b89acd01cbd82ee0a9fa80118038f91da988970c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-18 15:11:26 +0100

    Update CF 3.cpp

--- Vector/CF 3.cpp
+++ Vector/CF 3.cpp
@@ -185,3 +185,27 @@ void solve7()
     for(int i=0; i<result.size(); i++)
         cout << result[i] << " ";
 }
+int main(){
+    int t;
+    cin >> t;
+    vector<long long int> fact(1001);
+    fact[0] = 1;
+    long long int mod = 998244353;
+    
+    for(int i = 1; i <= 1000; i++){
+        fact[i] = (fact[i-1] * i) % mod;
+    }
+    
+    while(t){
+        int n;
+        cin >> n; 
+        
+        if(n % 2 == 1) cout << 0 << endl;
+        else{
+            cout << (fact[n/2] * fact[n/2]) % mod << endl;
+        }
+        t--;
+    }
+    
+    return 0;
+}
