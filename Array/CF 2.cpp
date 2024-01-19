b4a65ce011931685c49c12d1f6c5cfe7d66dab0c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-19 08:51:02 +0100

    Update CF 2.cpp

--- Array/CF 2.cpp
+++ Array/CF 2.cpp
@@ -46,4 +46,20 @@ void solve2() {
         cout << "NO" << endl;
     }
 }
-
+void solve3(){
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i = 0; i < n; i++){
+            cin >> arr[i];
+        }
+        int count = 0;
+        for(int i = 1; i < n; i++){
+            if(arr[i] == arr[i - 1]){
+                count++;
+                i++;
+            }
+        }
+    }
+    return 0;
+}
