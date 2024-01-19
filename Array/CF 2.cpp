2962479f04f0296281f9deaaf9f4d41ce7b2e400
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-19 08:56:07 +0100

    Update CF 2.cpp

--- Array/CF 2.cpp
+++ Array/CF 2.cpp
@@ -78,3 +78,25 @@ void solve3(){
     }
     return 0;
 }
+void solve4() {
+    int n;
+    cin >> n;
+    int a[n];
+    int b[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] > b[i])
+            swap(a[i], b[i]);
+    }
+    for (int i = 0; i < n - 1; i++) {
+        sum += abs(a[i + 1] - a[i]) + abs(b[i + 1] - b[i]);
+    }
+    cout << sum << endl;
+}
