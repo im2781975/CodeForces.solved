cebf3e91439b96ffbe45e5d4700cf7a7ca4bc811
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-19 05:47:06 +0100

    Update CF.cpp

--- Set/CF.cpp
+++ Set/CF.cpp
@@ -19,3 +19,27 @@ void solve_b_div4()
     else
         cout << "NO\n" ;
 }
+void solve_div3()
+{
+    set<char> unique_chars;
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string tmp = "";
+
+  // Convert all characters to lowercase
+    for (int i = 0; i < n; i++)
+        s[i] = tolower(s[i]);
+
+  // Create a string with unique characters
+    for (int i = 0; i < n; i++)
+        tmp += s[i];
+    tmp.erase(unique(tmp.begin(), tmp.end()), tmp.end());
+
+  // Check if the unique characters form the string "meow"
+    if (tmp == "meow")
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
