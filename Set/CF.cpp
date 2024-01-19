5e7b8a70fe77fc8c19218f3699c8e3ecf974e7ec
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-19 06:02:45 +0100

    Update CF.cpp

--- Set/CF.cpp
+++ Set/CF.cpp
@@ -89,3 +89,33 @@ void solve8()
         c = 1;
     cout << c + cpp << "\n";
 }
+void musical_puzzle()
+{
+    //https://codeforces.com/contest/1833/problem/A
+    int n;
+    int result = 0;
+    string str;
+    char temp;
+    cin >> n;
+    set<char> s({});
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        cin >> str;
+    
+        for (int j = 1; j < str.length(); j++)
+        {
+            temp = str[j] + str[j - 1];
+            cout << temp << endl;
+            s.insert(temp);
+        }
+        temp = 0;
+        for (int z = 0; z < s.size(); z++)
+        {
+            result++;
+        }
+        cout << result << "\n";
+    }
+}
