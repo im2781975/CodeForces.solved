da0ac3acf8b366e94cad5a1b2761396fbf959154
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-19 08:14:37 +0100

    Update CF.cpp

--- Map/CF.cpp
+++ Map/CF.cpp
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
 void solve9()
 {
     // https://codeforces.com/contest/1702/problem/D
@@ -30,4 +32,37 @@ void solve9()
             break;
     }
     cout << op << "\n";
+}
+void solve10()
+{
+    // https://codeforces.com/contest/1702/problem/C
+    int n, q;
+    cin >> n >> q;
+    // vector<int>stations(n+1);
+    // vector<pair<int, int>>queries(q+1);
+    int last = n - 1;
+    map<int, int> m;
+    map<int, int> m2;
+    map<int, int> l;
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        cin >> x;
+        m2[x]++;
+        if (m2[x] == 1)
+        {
+            m[x] = i;
+            l[x] = i;
+        }
+    }
+    for (int i = 1; i <= q; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (l[b] && m[a] && m[a] < l[b])
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
 }
