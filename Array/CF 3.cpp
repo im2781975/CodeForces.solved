aaf9bbddc1c276ef32c871606eaeb731398b8ff7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-19 12:02:47 +0100

    Update CF 3.cpp

--- Array/CF 3.cpp
+++ Array/CF 3.cpp
@@ -66,4 +66,53 @@ void solve3() {
     }
     cout << c;
 }
+void solve4(){
+    float y = 0, z[3], sum = 0;
+    int x = 0;
+
+    while (x++ < 4) {
+        cin >> y;
+        sum = sum + y;
+    }
+    int min = 0;
+    x = 0;
+
+    while (x++ < 3) {
+        cin >> z[x - 1];
+        if (z[min] > z[x - 1]) min = x - 1;
+    }
+
+    x = 0;
+    float f = 0;
+
+    while (x++ < 3) {
+        if (x - 1 == min) continue;
+        f += z[x - 1];
+    }
+
+    sum += f / 2;
+
+    if (sum >= 90) {
+        cout << "A";
+        return 0;
+    }
+    if (sum >= 80 && sum < 90) {
+        cout << "B";
+        return 0;
+    }
+    if (sum >= 70 && sum < 80) {
+        cout << "C";
+        return 0;
+    }
+    if (sum >= 60 && sum < 70) {
+        cout << "D";
+        return 0;
+    }
+    if (sum < 60) {
+        cout << "F";
+        return 0;
+    }
+
+    return 0;
+}
 
