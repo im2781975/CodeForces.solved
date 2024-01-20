1f5c15ef144a74e0c21544839770f86b0a55ad59
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-20 09:16:17 +0100

    Update Ex.cpp

--- Array/Ex.cpp
+++ Array/Ex.cpp
@@ -62,4 +62,28 @@ int main()
     cout << ans << endl;
     return 0;
 }
-
+#include<iostream>
+using namespace std;
+const int N = 1010;
+ 
+int t,n;
+int w[N],v[N];
+int f[N][N];
+int main()
+{
+	cin>>t>>n;
+	for(int i=1;i<=n;i++)
+	{
+		cin>>v[i]>>w[i];
+	}
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=0;j<=t;j++)
+		{
+			f[i][j]=f[i-1][j];
+			if(j>=v[i]) f[i][j]=max(f[i][j],f[i-1][j-v[i]]+w[i]);
+		}
+	} 
+	cout<<f[n][t];
+	return 0;
+} 
