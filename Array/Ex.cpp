379a9812a75c33e5a08ae23e86b3f382248bb470
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-21 18:29:14 +0100

    Update Ex.cpp

--- Array/Ex.cpp
+++ Array/Ex.cpp
@@ -459,3 +459,19 @@ for (int k = i; k <= j; k++)
    sum += a[k];
  }
 cout<<sum<<endl;
+----
+void extendgcd(ll a, ll b, ll*v)
+{
+    if (b == 0)
+    {
+        v[0] = 1;
+        v[1] = 0;
+        v[2] = a;
+        return ;
+    }
+    extendgcd(b, a % b, v);
+    ll x = v[1];
+    v[1] = v[0] - v[1] * (a / b);
+    v[0] = x;
+    return;//pass an arry of size1 3
+}
