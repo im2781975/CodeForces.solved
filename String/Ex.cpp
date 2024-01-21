42de26fa8c442bac592bb54a58184b4f7d3ed248
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-21 10:27:52 +0100

    Create Ex.cpp

---
+++ String/Ex.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	char op;
+	int n,s=0;
+	while(cin>>op,op!='@')
+	{
+		if(s) cout<<endl;
+		s=1;
+		cin>>n;
+		getchar();
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<i+n;j++)
+			{
+				if(j==n-1-i||j==n-1+i) cout<<op;
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	return 0;
+}
+//������:http://acm.hdu.edu.cn/showproblem.php?pid=2091
+#include <queue>
+#include <functional>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <assert.h>
+using namespace std;
+#define N 100005
+//�ӵ㣺ע����ĩ��Ҫ�пո� ע�����ͼ�μ任��
+char ch;
+int main()
+{
+   bool flag = 0;
+    while(scanf("%c",&ch),ch!='@'){  //������ʹ�� scanf("%c",ch) ���� getchar()  ��Ϊ��ȡ�߻س��� ������ scanf("%s",s) ���� cin>>sting ע����ʱ��Ҫ��"@"
+      if(flag) printf("\n");
+      flag = 1;
+      int n;
+      scanf("%d",&n);
+      getchar();                     //ȡ������n�� ���Ļس�
+      for(int i=0;i<n;i++){          //���ǱȽϼ���д��
+         for(int j=0;j<i+n;j++){
+            if(j==n-1-i || j==n-1+i || i==n-1) printf("%c",ch);
+            else printf(" ");
+         }
+         printf("\n");
+      }
+    }
+    return 0;
+}
+
+*
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int n,r;
+void solve(int n,int r)
+{
+	int f=1;
+	if(n<0) 
+	{
+		f=0;
+		n=-n;
+	}
+	string ans=""; 
+	while(n)
+	{
+		int a=n%r;
+		if(a==10) ans+='A';
+		else if(a==11) ans+='B';
+		else if(a==12) ans+='C';
+		else if(a==13) ans+='D';
+		else if(a==14) ans+='E';
+		else if(a==15) ans+='F';
+		else ans+=a+'0';
+		n/=r;
+	}
+	if(!f) cout<<"-";
+	for(int i=ans.size()-1;i>=0;i--)
+	{
+		cout<<ans[i];
+	}
+	cout<<endl;
+}
+
+int main()
+{
+	while(scanf("%d%d",&n,&r)!=EOF)
+	{
+		solve(n,r);
+	}
+	return 0;
+}
+*
