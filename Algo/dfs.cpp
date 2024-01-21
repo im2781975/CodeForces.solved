01acbaa22a464ce56c0ea037bceb4bcb8caf03f1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-21 13:32:24 +0100

    Update dfs.cpp

--- Algo/dfs.cpp
+++ Algo/dfs.cpp
@@ -52,3 +52,33 @@ void dfstemp(ll node,vector<vector<ll> > &adj,ll visited[],ll color[],ll* white,
     }
     return;
 }
+int recur(int node,int visited[],vector<vector<int> > m,int color[]){
+    visited[node]=1;
+    int coloring;
+    if(color[node]==1){
+        coloring=0;
+    }else{
+        coloring=1;
+    }
+    int counter=0;
+    for(int i=0;i<m[node].size();i++){
+        int child=m[node][i];
+        if(node==child){
+            counter=1;
+            continue;
+        }
+        if(!visited[child]){
+            color[child]=coloring;
+            int ans=recur(child,visited,m,color);
+            if(ans==1){
+                counter=1;
+            }
+        }else{
+            if(color[child]!=coloring){
+                counter=1;
+            }
+        }
+    }
+    // cout<<node<<" "<<counter<<endl;
+    return counter;
+}
