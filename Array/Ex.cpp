f75ffc009c44df51a34f0f2cb18b088c29811bf2
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-21 08:32:09 +0100

    Update Ex.cpp

--- Array/Ex.cpp
+++ Array/Ex.cpp
@@ -306,3 +306,40 @@ int main()
 	}
 	return 0;
 } 
+#include<iostream>
+using namespace std;
+
+int n;
+int a[33][33];
+
+void fun()
+{
+	a[1][1]=1;
+	a[2][1]=a[2][2]=1;
+	for(int i=3;i<=30;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			a[i][j]=a[i-1][j]+a[i-1][j-1];
+		}
+	}
+}
+
+int main()
+{
+	fun();
+	while(cin>>n)
+	{
+		for(int i=1;i<=n;i++)
+		{
+			cout<<a[i][1];
+			for(int j=2;j<=i;j++)
+			{
+				cout<<" "<<a[i][j];
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
