f09f160a8b866fbb657e9ff935ff796819e901af
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-21 22:08:51 +0100

    Update Ex 3.cpp

--- Base/Implement/Ex 3.cpp
+++ Base/Implement/Ex 3.cpp
@@ -89,3 +89,81 @@ return 0;
 
 }
 }
+int main() {
+
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int first = 0;
+    int last = 1;
+    for(int i = 0; i < n; i++) {
+
+        if(i % 2 == 1)
+            if(first == 1) {
+
+                printf("#");
+                first = 0;
+
+            }
+        for(int j = 0; j < m - i % 2; j++) {
+
+            if(i % 2 == 0) {
+
+                printf("#");
+
+            } else
+                printf(".");
+
+        }
+
+        if(i % 2 == 1)
+            if(last == 1) {
+
+                first = 1;
+                printf("#");
+                last = 0;
+
+            }
+
+        if(first == 0)
+            last = 1;
+        printf("\n");
+    }
+
+    return 0;
+
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int nr;
+
+    int odd = 0;
+    int even = 0;
+    int pos1 = -1;
+    int pos2 = -1;
+
+    for(int i = 0; i < n; i++) {
+
+        scanf("%d", &nr);
+        if(nr % 2 == 0) {
+
+            even++;
+            pos1 = i + 1;
+
+        } else {
+
+            odd ++;
+            pos2 = i + 1;
+
+        }
+    }
+
+    if(odd == 1)
+        printf("%d", pos2);
+    else
+        printf("%d", pos1);
+    return 0;
+}
