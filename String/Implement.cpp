ccf5d37dfe4c42e7175231417b98f90de0366792
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-21 08:28:56 +0100

    Update Implement.cpp

--- String/Implement.cpp
+++ String/Implement.cpp
@@ -1,4 +1,25 @@
 #include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	string s;
+	cin>>n;
+	while(n--)
+	{
+		cin>>s;
+		int ans=0;
+		for(int i=0;i<s.size();i++)
+		{
+			if(s[i]>='0'&&s[i]<='9') ans++;
+		}
+		cout<<ans<<endl;
+	}
+	return 0;
+}
+#include<iostream>
 #include<string>
 using namespace std;
 
