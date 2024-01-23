f55c503852a00222d79c25996e30cbba0672c860
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-23 21:50:04 +0100

    Update Ex 4.cpp

--- Base/Implement/Ex 4.cpp
+++ Base/Implement/Ex 4.cpp
@@ -65,3 +65,61 @@ int main()
        else
        cout<<"Friendship"<<endl;
 }
+int main()
+{
+ll a,b,c;
+        cin>>a>>b>>c;
+        cout<<min({a+b+c,2*a+2*b,2*a+2*c,2*b+2*c})<<endl;
+}
+int main()
+{
+     ll x,y;
+      cin>>x>>y;
+      if(y==1)
+      {
+        if(x==0)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+        return 0;
+     }   
+      y-=1;
+      x-=y;
+      if( y>=0 && x>=0 && x%2==0)
+      cout<<"Yes"<<endl;
+      else
+      cout<<"No"<<endl;
+}
+int main()
+{
+         
+      ll i,j,x=0,y=0,n,k=0;
+      cin>>n>>j;
+      x=n;
+      while(n--)
+      {
+                cin>>i;
+                if(i>j)
+                x++;
+      }
+      cout<<x<<endl;  
+}
+int main()
+{
+      ll n,a,b,c,d,e,f,g,h;
+      cin>>a>>b>>c>>d>>e>>f>>g>>h;
+      b*=c;
+      b/=g;
+      //cout<<b<<endl;
+      d*=e;
+      f/=h;
+     // cout<<f<<" "<<d<<endl;
+      cout<<min({b,f,d})/a<<endl;    
+}
+int main()
+{
+    ll a,b,c;
+      cin>>a>>b>>c;
+      b=a*b/__gcd(a,b);
+      cout<<c/b<<endl;
+}
