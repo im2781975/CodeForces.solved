5d35f7c815d4a46ef44b9a7d37de71848d4129fb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-30 15:26:09 +0100

    Update CF 6.cpp

--- Vector/CF 6.cpp
+++ Vector/CF 6.cpp
@@ -446,3 +446,37 @@ int main() {
 
     return 0;
 }
+void Matching Pair()
+{
+    //https://codeforces.com/problemset/problem/1788/C
+    int n;
+    cin >> n;
+    if(n%2==0)
+        cout << "No";
+    else
+    {
+        cout << "Yes";
+        vector<pair<int, int>> ans(n);
+        int nxt=2*n;
+        for(int i=0; i<n; i++)
+        {
+            ans[i].second=nxt;
+            nxt--;
+        }
+        nxt = 1;
+        for(int i=0; i<=n/2; i++)
+        {
+            ans[i].first=nxt;
+            nxt+=2;
+        }
+        nxt = 2;
+        for(int i=(n/2)+1; i<n; i++)
+        {
+            ans[i].first=nxt;
+            nxt+=2;
+        }
+        cout << "\n";
+        for(auto val:ans)
+            cout <<val.first << " " << val.second << "\n";
+    }
+}
