2b0584cd11ae9c4c253b846f139950da3761458b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-02-02 10:29:17 +0100

    Update CF 1.cpp

--- Array/CF 1.cpp
+++ Array/CF 1.cpp
@@ -280,3 +280,35 @@ void solve(){
         cout << "NO" << endl;
     }
 }
+//segment with small sum
+// the segment of the array is good if the sum of the elements of segment at most s;
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)
+        cin >> v[i];
+    int l=0, r=0, sum=0, ans=0;
+    while(r < n)
+    {
+        sum+=v[r];
+        if(sum <= k)
+        {
+            ans=max(ans, r-l+1);
+            r++;
+        }
+        else if(sum > k)
+        {
+            while(sum > k && l<=r)
+            {
+                sum-=v[l];
+                l++;
+            }
+            r++;
+        }
+    }
+    cout << ans;
+}
