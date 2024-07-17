c52329261db484e9424ff8318afb8b6bce3b6a6b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-17 22:26:29 +0200

    Update CF 10.cpp

--- Array/CF 10.cpp
+++ Array/CF 10.cpp
@@ -20,3 +20,49 @@ void FindMinimum(){
             cout << arr[i] << " ";
     }
 }
+
+//determine how many cells in a large grid fall within a certain 
+//Manhattan distance from multiple specified points. 
+const int N = 10000;
+int arr[N][N];
+void CountManhatten(){
+    int cnt = 0;
+    int x, y;
+    // Set the values for the first point (2020, 2020)
+    x = 2020; y = 2020;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(abs(i - x) + abs(y - j) <= 2020)
+                arr[i][j] = 1;
+        }
+    }
+    // Set the values for the 2nd point (4040, 2031)
+    x = 4040; y = 2031;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(abs(i - x) + abs(y - j) <= 2020)
+                arr[i][j] = 1;
+        }
+    }
+    x = 2031; y = 2034;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(abs(i - x) + abs(y - j) <= 2020)
+                arr[i][j] = 1;
+        }
+    }
+    x = 4020; y = 4020;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(abs(i - x) + abs(y - j) <= 2020)
+                arr[i][j] = 1;
+        }
+    }
+    for(int i =0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(arr[i][j] == 1)
+                cnt++;
+        }
+    }
+    cout << cnt;
+}
