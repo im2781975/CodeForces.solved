483d7fef94857166fdf29a666a141d6c04a6cc50
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-18 17:49:31 +0200

    Update ex.cpp

--- Vector/ex.cpp
+++ Vector/ex.cpp
@@ -20,3 +20,14 @@ void MaxDiff(){
     sort(cev.begin(), cev.end());
     cout << "\nDiffrence is: " <<  (vec[mid -1] + cev[mid - 1]) - (vec[0] + cev[0]);
 }
+
+void MultMaxInteger(){
+    int n; cin >> n;
+    vector <int> vec(n);
+    for(int i = 0; i < n; i++)
+        cin >> vec[i];
+    sort(vec.begin(), vec.end());
+    int x = vec[n - 1] * vec[n - 2];
+    int y = vec[0] * vec[1];
+    (x >= y)?cout << x :cout << y;
+}
