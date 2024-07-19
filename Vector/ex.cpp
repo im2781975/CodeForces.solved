7ccf9ed7797018d0a9d1c33b945fdd041e95c186
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-19 09:23:48 +0200

    Update ex.cpp

--- Vector/ex.cpp
+++ Vector/ex.cpp
@@ -51,3 +51,39 @@ void Manipulate(){
     cout << "\ncnt: " << cnt + 1;
 }
 //****//
+//finding the minimum sum of two integers first corresponds to the "01" in the string &
+//the other corresponds "10" in the string. If both occurrences are found, 
+//it calculates & prints the minimum sum & compare with other string "11". otherwise, it prints -1.
+void MinSum() {
+    int n, cnt01 = 0, cnt10 = 0, found01 = 0, found10 = 0, cont01 = INT_MAX, cont10 = INT_MAX, tmp2 = INT_MAX, tmp1 = INT_MAX;
+    // tmp1 is used to store the minimum integer associated with the string "11".
+    // tmp2 will be used to store the min sum of integers associated with "01" and "10"
+    cin >> n;
+    vector<pair<int, string>> vec(n);
+    for(int i = 0; i < n; i++)
+        cin >> vec[i].first >> vec[i].second;
+        
+    for(int i = 0; i < n; i++) {
+        if(vec[i].second == "01") {
+            cnt01++;
+            cont01 = min(cont01, vec[i].first);
+            found01 = 1;
+        }
+        else if(vec[i].second == "10") {
+            cnt10++;
+            cont10 = min(cont10, vec[i].first);
+            found10 = 1;
+        }
+        else if(vec[i].second == "11") {
+            cnt01++; cnt10++;
+            tmp1 = min(tmp1, vec[i].first);
+        }
+    }
+    if(found01 > 0 && found10 > 0)
+        tmp2 = min(cont01 + cont10, tmp2);
+    
+    if(tmp1 == INT_MAX && tmp2 == INT_MAX)
+        cout << -1;
+    else
+        cout << min(tmp1, tmp2);
+}
