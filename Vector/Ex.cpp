89d1cdc0e5676760601237a7728ad9cfa21fb4da
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-19 13:38:28 +0200

    Update Ex.cpp

--- Vector/Ex.cpp
+++ Vector/Ex.cpp
@@ -50,3 +50,22 @@ void MaxProduct(){
     }
     cout << maxi;
 }
+
+void PrimeFactor(){
+    int n; cin >> n;
+    vector <int> prime;
+    if(n % 2 == 0){
+        prime.push_back(2);
+        while(n % 2 == 0)
+            n /= 2;
+    }
+    for(int i = 3; i * i <= n; i +=2){
+        prime.push_back(i);
+        while(n % i == 0)
+            n/= i;
+    }
+    if(n > 2)
+        prime.push_back(n);
+    for(int i = 0; i < prime.size(); i++)
+        cout << prime[i] << " ";
+}
