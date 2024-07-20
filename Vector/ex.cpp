38d0e7b06a6e08e03fd6cf95240bcbd5a6335fee
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-20 17:09:40 +0200

    Update ex.cpp

--- Vector/ex.cpp
+++ Vector/ex.cpp
@@ -16,3 +16,27 @@ void PrimeFactor(){
     for(int i = 0; i < prime.size(); i++)
         cout << prime[i] << " ";
 }
+
+// checks if the vector is sorted in increasing order. If it is, calculates the minimim diff 
+//between consecutive elements and outputs half of this difference plus one. If not sorted, outputs 0.
+void ConsecutiveDiff(){
+    int n; cin >> n;
+    vector <int> vec(n);
+    bool flag = true;
+    int tmp = INT_MAX;
+    for(int i = 0; i < n; i++)
+        cin >> vec[i];
+    for(int i = 1; i < n; i++){
+        if(vec[i] < vec[i-1]){
+            flag = false;
+            break;
+        }
+    }
+    if(flag){
+        for(int i = 1; i < n; i++)
+            tmp = min(tmp, vec[i] - vec[i - 1]);
+        cout << tmp/2 + 1;
+    }
+    else
+        cout << 0;
+}
