b3b374d95d700de578905779f7b75aaa319ea781
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-23 00:54:09 +0200

    Update Operation.cpp

--- Base/Implement/Operation.cpp
+++ Base/Implement/Operation.cpp
@@ -1,37 +1,6 @@
 #include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
-int c=0;
-int fact(long long int x) {
-    if (x == 0 || x == 1)
-        return 1;
-    return x * fact(x - 1);
-}
-int countDigits(int x) {
-    while (x != 0) {
-        x = x / 10;
-        c++;
-    }
-    return c;
-}
-int luck(int x)
-{
-    int y[]={4, 7, 47, 74, 444, 447, 474, 477, 744, 747, 774, 777};
-    for(int i =0; i <12 && y[i] <=x; i++)
-    {
-        if(x % y[i] == 0)
-            return 1;
-    }
-    return 0;
-}
-int rev(int n)
-{
-    int odd;
-    if( n==0)
-        return odd;
-    odd=(n/10)+(n%10)*pow(10, c++);
-}
 int Binary(int n)
 {
     while(n > 0)
