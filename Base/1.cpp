4b02a9775dcac13e93be655033fcbc997ae3b54e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-24 20:44:53 +0200

    Update 1.cpp

--- Base/1.cpp
+++ Base/1.cpp
@@ -165,3 +165,93 @@ void IsEven(){
     }
     cout << "No";
 }
+
+//Simulate two scheduling algorithms for process management in operating systems
+//: First-Come, First-Served (FCFS) and Shortest Job First (SJF) non-preemptive.
+void Simulate() {
+    int num;
+    // Represent the num of programs
+    cin >> num;
+    if (num <= 0) {
+        cout << "Should be Greater than zero" << endl;
+        return 1;
+    }
+    float *arrTime = new float[num];
+    float *executeTime = new float[num];
+    int i = 0;
+    while (i < num) {
+        cout << "\nArrival & Exit of programme " << i + 1 << " is: ";
+        cin >> arrTime[i];
+        cin >> executeTime[i];
+        //execute time represent the finishing time while the code finished
+        i++;
+    }
+    // FCFS -> First Come First Served
+    // SJF -> Shortest Job First (non-preemptive)
+    cout << "Enter 1 for FCFS or 2 for SJF-non preemptive: ";
+    int choice;
+    cin >> choice;
+    if (choice == 1) {
+        float curTime = 0, turnTime = 0, waitTime = 0;
+        float totalTurnTime = 0, totalWaitTime = 0;
+        //curTime ->when the code finished for execution
+        //arrTime ->when the code start for execution
+        // TurnTime is the total time taken from arrival to completion.
+        // WaitTime (T(w)) is the total time a program spends waiting in the ready queue.
+        for (i = 0; i < num; i++) {
+            if (curTime < arrTime[i]) {
+                curTime = arrTime[i]; // Process starts at the arrival time if the CPU is idle
+            }
+            curTime += executeTime[i];
+            turnTime = curTime - arrTime[i];
+            waitTime = turnTime - executeTime[i];
+            cout << "The Turnaround time (T(a)) of program " << i + 1 << " = " << turnTime << endl;
+            cout << "The Waiting time (T(w)) of program " << i + 1 << " = " << waitTime << endl;
+            totalTurnTime += turnTime;
+            totalWaitTime += waitTime;
+        }
+        cout << "Average Turnaround time (T(a)) = " << totalTurnTime / num << endl;
+        cout << "Average Waiting time (T(w)) = " << totalWaitTime / num << endl;
+    } else if (choice == 2) {
+        int curTime = 0;
+        // store the time at which each process finishes execution. The index of the array corresponds to the process number.
+        float *completionTime = new float[num];
+        //array of booleans to keep track of whether each process has been processed or not.initially declared false
+        bool *processed = new bool[num]();
+        for (int i = 0; i < num; ++i) {
+            int minIdx = -1;
+            for (int j = 0; j < num; ++j) {
+                if (!processed[j] && arrTime[j] <= curTime && 
+                   (minIdx == -1 || executeTime[j] < executeTime[minIdx])) {
+                    minIdx = j;
+                }
+            }
+            if (minIdx != -1) {
+                processed[minIdx] = true;
+                curTime += executeTime[minIdx];
+                completionTime[minIdx] = curTime;
+            } else {
+                // If no process is ready to execute, advance the current time to the next arrival time
+                curTime++;
+                i--; // Retry current iteration as no process was executed
+            }
+        }
+        float totalTurnTime = 0, totalWaitTime = 0;
+        for (int i = 0; i < num; ++i) {
+            float turnTime = completionTime[i] - arrTime[i];
+            float waitTime = turnTime - executeTime[i];
+            cout << "The Turnaround time (T(a)) of program " << i + 1 << " = " << turnTime << endl;
+            cout << "The Waiting time (T(w)) of program " << i + 1 << " = " << waitTime << endl;
+            totalTurnTime += turnTime;
+            totalWaitTime += waitTime;
+        }
+        cout << "Average Turnaround time (T(a)) = " << totalTurnTime / num << endl;
+        cout << "Average Waiting time (T(w)) = " << totalWaitTime / num << endl;
+        delete[] completionTime;
+        delete[] processed;
+    } else
+        cout << "Invalid Choice";
+    delete[] arrTime;
+    delete[] executeTime;
+    return 0;
+}
