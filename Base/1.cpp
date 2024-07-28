be77f05f63302d183a58389eb127ec2c2e1c3c65
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-28 08:27:58 +0200

    Update 1.cpp

--- Base/1.cpp
+++ Base/1.cpp
@@ -220,3 +220,35 @@ void CountGreater(){
     }
     cout << high << " " << low;
 }
+
+//BubbleSort with count swaps
+void BubbleSort(){
+    int n; cin >> n;
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    int Ind = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] < arr[i + 1])
+            continue;
+        else
+            Ind = -1;
+    }
+    if(Ind == 0){
+        cout << "Ordered";
+        return 0;
+    }
+    else{
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j < n - 1 - i; j++){
+                if(arr[j] > arr[j + 1])
+                    swap(arr[j], arr[j + 1]);
+                    ++Ind;
+            }
+        }
+    }
+    cout << Ind << "\n";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    delete []arr;
+}
