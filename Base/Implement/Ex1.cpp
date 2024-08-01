4744afe68c21bdabc3906cabd8bfdc9d881821b6
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-01 22:31:28 +0200

    Update Ex1.cpp

--- Base/Implement/Ex1.cpp
+++ Base/Implement/Ex1.cpp
@@ -1,34 +1,4 @@
-void solve()
-{
-    unsigned long long int m,n,a,N,M;
-    cin>>m>>n>>a;
-    M = m/a;
-    N = n/a;
-    if (m%a!=0)
-        M++;
-    if (n%a!=0)
-        N++;
-    unsigned long long int ans = M*N;
-    cout<<ans;
-}
-void solve()
-{
-    int n;
-    cin>>n;
-    if (n%2==0 && n!=2)
-        cout<<"YES";
-    if (n%2==1 || n==2)
-        cout<<"NO";
-}
-void solve()
-{
-    long int s=11;
-    cout<<letter_count(911);
-    for (int i = 1;i<=999;i++)
-        s+= letter_count(i);
-    cout<<endl<<s;
 
-}
 int MultOfNumbers(int n)
 	{
 		int mult = 1;
