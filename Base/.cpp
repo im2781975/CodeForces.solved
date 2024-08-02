ad00c249f023bd6f45152a3ef82581e0875a5ed3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-02 22:15:59 +0200

    Update .cpp

--- Base/.cpp
+++ Base/.cpp
@@ -42,3 +42,104 @@ void Condition(){
     }
     cout << curr << "\n";
 }
+//determine whether it is possible to perform a series of jumps between two integers
+//a and b, using a fixed step size c, such that the total number of jumps is even.
+void CountJump(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int j = abs(a - b);
+    if(j % c != 0){
+        cout << "No";
+        continue;
+    }
+    j /= c;
+    if(j % 2 == 0)
+        cout << "Yes";
+    else
+        cout << "No";
+}
+//purpose of this program is to simply compare two numbers and output which one is larger, 
+void CompareBetween(){
+    int x = 0, y = 0;
+    int a, b; cin >> a >> b;
+    (a > b) ? x++: y++;
+    if(x > y) cout << "A";
+    else if(y > x) cout << "B";
+    else
+        cout << "Equal";
+}
+//decompose a given integer a into the smallest number of parts such that each part 
+//is either the integer 2 or 3. It then prints the number of parts followed by the actual parts themselves
+void divideIntoParts(){
+    int n, j; 
+    cin >> n;
+    if(n % 2 == 0){
+        cout << n / 2;
+        j = n / 2;
+        while(j--){
+            cout << 2 << " ";
+        }
+    }
+    else {
+        cout << (a - 3)/2 + 1;
+        j = (a - 3)/2;
+        cout << 3 ;
+        while(j--){
+            cout << 2 << " ";
+        }
+    }
+}
+//How much steps we need for being two variable same
+void Same(){
+    int x, y;
+    cin >> x >> y;
+    if(x == y)
+        cout << 0;
+    else if(x > y)
+        cout << x - y;
+    else {
+        int Operation = 0;
+        while(y > x){
+            if(y % 2 == 0)
+                y /= 2;
+            else
+                y++;
+            Operation++;
+        }
+        Operation += x - y;
+        cout << Operation;
+    }
+}
+void Compare(){
+    int x, y;
+    cin >> x >> y;
+    if(y == 1){
+        if(x == 0)
+            cout << "Yes";
+        else
+            cout << "No";
+        return 0;
+    }
+    y -= 1;
+    x -= y;
+    (x >= 0 && y >= 0 && x % 2 == 0) ? cout << "Yes": cout << "No";
+}
+//compute and print the minimum value from a set of given expressions 
+void Calculate(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << min({a + b + c, 2 *(a + b), 2 * (b + c), 2 * (c + a)});
+}
+//compare two calculated values to determine which of two options is better, or if they are equivalent. 
+void Compare(){
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    b *= a; b += 2 * d;
+    c *= a; c += 2 * e;
+    if(b > c)
+        cout << "second";
+    else if(c > b)
+        cout << "First";
+    else
+        cout << "Friendship";
+}
