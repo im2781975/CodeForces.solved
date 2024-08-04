957c48cce84f6f661f7afa31fc0d1d4c0b98d5e7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-04 11:07:07 +0200

    Update Ex 1.cpp

--- Base/Implement/Ex 1.cpp
+++ Base/Implement/Ex 1.cpp
@@ -1,47 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <map>
-#include <queue>
-#include <algorithm>
-#include <valarray>
-#include <cmath>
-#include <numeric>
-#include <iostream>
-#include<set>
-#include<map>
-#include<stack>
-#include<queue>
-#include<vector>
-#include<cstdio>
-#include<string>
-#include<cstring>
-#include<cstdlib>
-#include<iostream>
-#include<algorithm>
-#include<cmath>
-#include<climits>
-#define forI(n) for(ll i = 0; i < n; ++i)
-#define forJ(n) for(ll j = 0; j < n; ++j)
-#define forR(n) for(ll i = n - 1; i >= 0; --i)
-#define forRangeI(l, r) for(ll i = l; i <= r; ++i)
-#define forRRangeI(l, r) for(ll i = r; i >= l; --i)
-#define forRangeJ(l, r) for(ll j = l; i <= r; ++i)
-#define forRRangeJ(l, r) for(ll j = r; i >= l; --i)
-#define forEach(array) for(auto &item : array)
-#define forEach1(array) for(auto &item1 : array)
-#define printSpace(a) cout << a << " "
-#define printSpace2(a, b) cout << a << " " << b << " "
-#define printSpace3(a, b, c) cout << a << " " << b << " " << c << " "
-#define printSpace4(a, b, c, d) cout << a << " " << b << " " << c << " " << d <<" "
-#define printSpace5(a, b, c, d, e) cout << a << " "<< b << " " << c << " " << d <<" " << e << " "
-#define printSpace6(a, b, c, d, e, f) cout << a << " "<< b << " " << c << " " << d <<" "<< e << " " << f << " "
-#define printLn(n) cout << n << '\n'
-#define printNL() cout <<'\n'
-#define print(n) cout << n
-
 using namespace std;
-
 typedef long long ll;
 typedef unsigned long long ull;
 
