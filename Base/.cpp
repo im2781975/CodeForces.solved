d7553ab3f74344c04d185cc95983215dfaabc335
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-08-04 20:58:27 +0200

    Update .cpp

--- Base/.cpp
+++ Base/.cpp
@@ -34,6 +34,65 @@ void checkQuality(){
     cout << "Poor";
     return 0;
 }
+//Compressed Letter
+struct CompressedLetter{
+    int num;
+    char letter;
+};
+vector <CompressedLetter> Process(const string &str){
+    vector <CompressedLetter> res;
+    int pos = 0;
+    while(pos < str.length()){
+        int len = 0;
+        while(str[pos]>= '0' && str[pos] <= '9'){
+            len = len * 10 + (str[pos] - '0');
+            pos++;
+        }
+        if(len == 0)
+            len = 1;
+        if(pos < str.length()){
+            res.push_back({len, str[pos]});
+            pos++;
+        }
+    return res;
+}
+//Sum Between Ranges
+int SumRanges(int l, int r){
+    if(r < l)
+        return 0;
+    if(l > 1)
+        return SumRanges(1, r) - SumRanges(1, l - 1);
+    if(r % 2 == 1)
+        return SumRanges(1, r - 1) + r;
+    else
+        return r * (r + 1)/2;
+}
+
+//compute specific values related to the movement or relationship between two points on a 2D grid
+void GridMove(){
+    int a, b, c, d; cin >> a >> b >> c >> d;
+    int IsDiffrent = (a != c && b != d) ? 2 : 1;
+    int SameParity = ((a + b) % 2 == (c + d) % 2) ? 1 : 0;
+    int DiffrentDirection = (a - b == c - d || a + b == c + d) ? 1 : 0;
+    int res = IsDiffrent * (2 - DiffrentDirection);
+    int maxDist = max(abs(c - a), abs(d - b));
+    cout << res << " " << SameParity * res << " " << maxDist ;
+}
+//determine how many times each die wins, and how many times there is a draw, 
+// when compared to a series of possible dice outcomes (from 1 to 6).
+void CompareDice(){
+    int dice1, dice2; cin >> dice1 >> dice2;
+    int dice1Win = 0, dice2Win = 0, draw = 0;
+    for(int i = 1; i <= 6; i++){
+        if(abs(dice1 - i) > abs(dice2 - i))
+            dice2Win++;
+        else if(abs(dice1 - i) < abs(dice2 - i))
+            dice1Win++;
+        else
+            draw++;
+    }
+    cout << dice1Win << " " << draw << " " << dice2Win;
+}
 void verify(){
     int n, k; cin >> n >> k;
     int maxi = INT_MIN;
@@ -355,4 +414,250 @@ void Compare(){
         cout << "First";
     else
         cout << "Friendship";
+}
+
+int MinPos(vector <int> &vec){
+    int pos = 0;
+    for(int i = 1; i < vec.size(); i++){
+        if(vec[i] < vec[pos])
+            pos = i;
+    }
+    return pos;
+}
+int MaxPos(vector <int> &vec){
+    int pos = 0; 
+    for(int i = 1; i < vec.size(); i++){
+        if(vec[i] > vec[pos])
+             pos = i;
+    }
+    return i;
+}
+vector <int> PrefixSum(vector <int> &vec){
+    vector <int> prefix(vec.size());
+    prefix[0] = vec[0];
+    for(int i = 1; i < vec.size(); i++){
+        prefix[i] = prefix[i - 1] + vec[i];
+    }
+    return prefix;
+}
+vector <int> SuffixSum(vector <int> &vec){
+    vector <int> suffix(vec.size());
+    suffix[vec.size() - 1] = vec[vec.size() - 1];
+    for(int i = vec.size() - 2; i >= 0; i--)
+        suffix[i] = suffix[i + 1] + vec[i];
+    return suffix;
+}
+//DetectPrime
+vector <bool> DetectPrime(int n){
+    vector <bool> num( n + 1, true);
+    num[0] = false; num[1] = false;
+    for(int i = 2; i <= n; i++){
+        if(num[i]){
+            for(int j = i*i; j <= n; j+= i){
+                num[j] = false;
+            }
+        }
+    }
+    return num;
+}
+vector <int> GetDigit(int n){
+    vector <int> Digits;
+    while(n > 0){
+        Digits.push_back(n % 10);
+        n /= 10;
+    }
+    return Digits;
+}
+__main(){
+    int n; cin >> n;
+    vector <bool> prime = DetectPrime(n);
+    vector <int> Get = GetDigit(n);
+    
+    for(int i = 2; i <= n; i++){
+        if(prime[i])
+            cout << i << " ";
+    }
+    cout << "\n";
+    for(int i = 0; i < Get.size(); i++)
+        cout << Get[i] << " ";
+}
+// count and output the number of elements in the vector that are greater than or
+//equal to the maxi value between 1 and the smallest element in the sorted vector.
+void Count(){
+    int n; cin >> n;
+    int k = n - 1;
+    vector <int>vec(n);
+    for(int i = 0; i < n; i++){
+        int x; cin >> x;
+        vec[i] = x;
+    }
+    sort(vec.begin(), vec.end(), greater <int>());
+    int min = max(1, vec[k]);
+    int res = 0;
+    for(int i = 0; i < n; i++){
+        if(vec[i] >= mini)
+            res++;
+    }
+    cout << res;
+}
+
+//determine if a given integer will eventually converge to 1 when repeatedly replaced by the sum of the squares of its digits. 
+int SquareSum(int num){
+    int sum = 0;
+    while(num > 0){
+        int digit = num % 10;
+        sum += digit * digit;
+        num /= 10;
+    }
+    return sum;
+}
+int convergence(int num, int maxiit){
+    int cur = num, original = num, iteration = 0;
+    do {
+        cur = SquareSum(cur);
+        if(cur == 1)
+            return 1;
+        if(cur == original)
+            return 0;
+        iteration++;
+    }while(iteration < maxiit);
+    return 0;
+}
+main(){
+    int num; cin >> num;
+    int res = convergence(num, 100);
+    (res == 1)?cout << num << " covergence to 1":cout << num << " Enters into a cycle or not convergence";
+}
+
+//Given Value n with number 1.can multiply either 10 or 20 with n.check is it can reach the output value
+bool ReachValue(int cur, int trg){
+    if(cur == trg)
+        return true;
+    if(cur > trg)
+        return false;
+    return ReachValue(cur *10, trg) || ReachValue(cur *20, trg);
+}
+main(){
+    int n; cin >> n;
+    (ReachValue(1, n))? cout << "Yes":cout << "No";
+}
+
+void verify(){
+    int x, y; cin >> x >> y;
+    if(x == 1 && y == 1)
+        cout << 0;
+    else if(x == 1 && y == 2 || x == 2 && y == 1)
+        cout << 1;
+    else if(x == 1 && y > 2 || x > 2 && y == 1)
+        cout << 2;
+    else {
+        if((x % 2 == 0 && y % 2 == 0) || (x % 2 == 1 && y % 2 == 1))
+            cout << (max(x, y) - 1)*2;
+        else 
+            cout << (max(x, y) - 1)*2 - 1;
+    }
+}
+void verify(){
+    int n; cin >> n;
+    int ans = 0;
+    int x = n % 10 - 1;
+    ans += 10 * x;
+    int k = n % 10;
+    int cnt = 0, ptr = 1;
+    while(cnt <= n){
+        cnt = cnt % 10 + k;
+        ans += ptr; ptr++;
+    }
+    cout << ans;
+}
+void BasedCondition(){
+    int a, b, ans;
+    cin >> a >> b;
+    (a == 0) ? ans = 1 : ans = a + 2 * b + 1;
+    cout << ans;
+}
+//GCD, LCM
+int GCD(int a, int b){
+    if(a == 0 || b == 0)
+        return a + b;
+    if(a > b)
+        return (a % b, b);
+    return (a, b % a);
+}
+int GCD(int a, int b){
+    if(b > a)
+        swap(b, a);
+    if(a == b)
+        return a;
+    for(int i = b; i > 0; i--){
+        if(b % i == 0 && a % i == 0)
+            return i;
+    }
+}
+int LCM(int a, int b){
+    for(int i = max(a, b); i <= a*b; i++){
+        if(i % a == 0 && i % b == 0)
+            return i;
+    }
+    return -1;
+}
+__main(){
+    int n, x; cin >> n >> x;
+    cout << GCD(n, x);
+}
+//Hanoi
+void Hanoi(int n, char a, char b, char c){
+    if(n > 0){
+        Hanoi(n - 1 , a, c, b);
+        cout << "Move From " << a << " To " << b << " Use " << c << "\n";
+        Hanoi(n - 1, c, b, a);
+    }
+}
+__main(){
+    int n; cin >> n;
+    char a, b, c; cin >> a >> b >> c;
+    Hanoi(n, a, b, c);
+}
+//Party Arrange
+void PartyArrange(){
+    int Friends, Bottles, mlPerBottle, Lime, SlicePerLime, SaltAmount, mlPerServing, SaltPerServing;
+    cin >> Friends >> Bottles >> mlPerBottle >> Lime >> SlicePerLime >> SaltAmount >> mlPerServing >> SaltPerServing;
+    int TotalML = (Bottle * mlPerBottle)/mlPerServing / Friends;
+    int TotalSalt = SaltAmount / SaltPerServing / Friends;
+    int TotalLime = (Lime * SlicePerLime) / Friends;
+    int minServing = min({TotalML, TotalSalt, TotalLime});
+    cout << minServing;
+}
+
+void verifyCond(){
+    int x, y; cin >> x >> y;
+    int res = 0;
+    if(y > x && x >= -y){
+        cout << "a";
+        res = -2 + y * 4;
+    }
+    else if(x > y && x <= -y + 1){
+        cout << "b";
+        res = -y * 4;
+    }
+    else if(y <= x && x > -y + 1){
+        cout << "c";
+        res = -3 + x * 4;
+    }
+    else if(y >= x && x < -y){
+        cout << "d";
+        res = -1 - 4 * x;
+    }
+    cout << res;
+}
+
+//Print Even Between range
+void PrintBetween(){
+    int l, r; cin >> l >> r;
+    if(l % 2 != 0)
+        l += 1;
+    if(r - l >= 4)
+        cout << l << " " << l + 2 << " " << l + 4;
+    else
+        cout << -1;
 }
