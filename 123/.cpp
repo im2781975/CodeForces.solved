e22758539d417c734f55fdc8cd33eef0c7ac827a
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-10 17:13:37 +0100

    Update .cpp

--- 123/.cpp
+++ 123/.cpp
@@ -20,3 +20,25 @@ void PenguinAndSegments(){
     else
         cout << k - (sum % k);
 }
+using namespace std;
+void LuckyTicket(){
+    int n, sum1 = 0, sum2 = 0;
+    string str; cin >> n >> str;
+    for(int i = 0; i < n / 2; i++){
+        if(str[i] == '7' || str[i] == '4')
+            sum1 += str[i] - '0';
+        else{
+            cout << "No";
+            return 0;
+        }
+    }
+    for(int i = n - 1; i >= n / 2; i--){
+        if(str[i] == '7' || str[i] == '4')
+            sum2 += str[i] - '0';
+        else{
+            cout << "No";
+            return 0;
+        }
+    }
+    (sum1 == sum2) ? cout << "Yes" : cout << "No";
+}
