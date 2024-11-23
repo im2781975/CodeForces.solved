a5222437e8f53cb4a582e5bed8cc1f306dd81f94
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-23 20:06:00 +0100

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,4 +1,33 @@
 using namespace std;
+void Juicer(){
+    int n, b, d; cin >> n >> b >> d;
+    vector <int> vec(n);
+    int cnt = 0, sum = 0;
+    for(int i = 0; i < n; i++){
+        cin >> vec[i];
+        if(vec[i] <= b)
+            sum += vec[i];
+        if(sum > d){
+            cnt++;
+            sum = 0;
+        }
+    }
+    cout << cnt;
+}
+using namespace std;
+void YoungPhysicist(){
+    int n; cin >> n;
+    int sumx = 0, sumy = 0, sumz = 0;
+    while(n--){
+        int x, y, z; cin >> x >> y >> z;
+        sumx += x; sumy += y; sumz += z;
+    }
+    if(sumx == 0 && sumy == 0 && sumz)
+        cout << "Yes";
+    else
+        cout << "No";
+}
+using namespace std;
 void SirajaAndDima(){
     int n; cin >> n;
     deque <int> dq(n);
